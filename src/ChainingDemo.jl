be7353526301bfa999b9a0227d62f0033ddc27ce
uniment <uniment@gmail.com>
2022-11-19 19:43:10 -0800

    logic operators

--- src/ChainingDemo.jl
+++ src/ChainingDemo.jl
@@ -6,7 +6,7 @@ include("fix.jl")
 include("underscore.jl")
 include("whitelist.jl")
 
-export @demo_str, @underscores, @chain, @chainlink, Fix, Fix1, Fix2, FixFirst, FixLast, ChainLink
+export @demo_str, @underscores, @chain, @chainlink, Fix, Fix1, Fix2, FixFirst, FixLast, ChainLink, ∨, ∧, ¬
 
 
 
